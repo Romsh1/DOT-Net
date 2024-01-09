c77cc33e3b13f997015bcbc712e87af8b7dc4fcb
Romika Chaudhary <152133936+Romsh1@users.noreply.github.com>
2024-01-09 13:50:03 -0500

    Add files via upload
    A demo program to calculate average of two numbers

---
+++ calcAvg/calcAvg/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcAvg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 356;
+            int num = 3;
+
+            double average = sum / num;
+
+            Console.WriteLine(average);
+            Console.ReadLine();
+
+        }
+    }
+}
