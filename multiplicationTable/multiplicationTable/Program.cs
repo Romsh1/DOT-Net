a0c7f9797111b1d3cd2c532324be4ec5230a0a32
Romika Chaudhary <152133936+Romsh1@users.noreply.github.com>
2024-01-20 00:24:34 -0500

    Add files via upload

---
+++ multiplicationTable/multiplicationTable/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int horzStart, horzEnd;
+            int vertStart, vertEnd;
+
+            /* (11, 13) */
+            horzStart = 11;
+            horzEnd = 13;
+
+           /* ( 3, 6) */
+            vertStart = 3;
+            vertEnd = 6;
+
+            Console.Write("{0, -4", "X");
+
+            for(int col = horzStart; col <= horzEnd; col++)
+            {
+                Console.Write("{0, -4", col);
+            }
+            Console.WriteLine();
+
+            for (int row = vertStart; row <= vertEnd; row++)
+            {
+                Console.Write("{0, -4}", row);
+                for(int col = horzStart; col <= horzEnd ; col++)
+                {
+                    Console.Write("{0, -4}", row * col);
+                }
+                Console.WriteLine() ;
+            }
+        }
+    }
+}
